de7502da3c0efdb15258cfb70fc63dbdb800744f
greyjediak <greyjediak@gmail.com>
2026-05-07 18:04:36 -0500

    added package and express cors

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express(); // Create instance of express
+const PORT = 6000;
+
+app.use(cors());
+app.use(express.json());
+
+let posts = [];
+
+// Test
+app.get("/", (req, res) => {
+    res.send("Correct server.js is running");
+});
